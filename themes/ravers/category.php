a7873c458a7954fbd6eb70bde4e87a1a94b32871
Luis Abanto <lfam200@hotmail.com>
2016-01-19 11:58:53 -0500

    wsp3

--- themes/ravers/category.php
+++ themes/ravers/category.php
@@ -84,66 +84,36 @@
               
           // Por ejemplo, imprimimos el nombre de las subcategorias
           foreach ($categories as $category) : 
-              
+              $name = $category->name;
           ?>
 
-            <div class="cat col-xs-4 texto-blanco text-left"><?php echo $category->name;?></div>
+            <div class="cat col-xs-4 texto-blanco text-left"><?php echo $name;?></div>
             <div class="title_border col-xs-12"></div>
             <div class="col-xs-12">
               <!-- Start the Loop. -->
-               <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-                <!-- Test if the current post is in category 3. -->
-                <!-- If it is, the div box is given the CSS class "post-cat-three". -->
-                <!-- Otherwise, the div box is given the CSS class "post". -->
-
-                <?php if ( in_category( 'Water accesability projects' ) ) : ?>
-                  <div class="post-cat-three">
+              <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <?php if ( in_category( $category->slug ) ) : ?>
+                  <div class="post">
+                    <h2>
+                      <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                    <div class="entry">
+                      <?php the_excerpt(); ?>
+                    </div>
+                    <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+                </div>
                 <?php else : ?>
                   <div class="post">
+                  </div>
                 <?php endif; ?>
-
-
-                <!-- Display the Title as a link to the Post's permalink. -->
-
-                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
-
-                <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-
-                <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-
-
-                <!-- Display the Post's content in a div box. -->
-
-                <div class="entry">
-                  <?php the_content(); ?>
-                </div>
-
-
-                <!-- Display a comma separated list of the Post's Categories. -->
-
-                <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
-                </div> <!-- closes the first div box -->
-
-
-                <!-- Stop The Loop (but note the "else:" - see next line). -->
-
-               <?php endwhile; else : ?>
-
-
+              <?php endwhile; else : ?>
                 <!-- The very first "if" tested to see if there were any Posts to -->
                 <!-- display.  This "else" part tells what do if there weren't any. -->
                 <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-
-
                 <!-- REALLY stop The Loop. -->
                <?php endif; ?>
             </div>
-            
-          <?php  
-          endforeach;
-          ?>
+          <?php  endforeach; ?>
           </div> 
         </div> 
 
