bd924f92141067922e0e5ae55ece1ad02ea5ee61
Luis Abanto <lfam200@hotmail.com>
2016-01-19 20:01:10 -0500

    wsp13

--- themes/ravers/single.php
+++ themes/ravers/single.php
@@ -34,25 +34,11 @@
             <?php endif; ?>
             
       </div> 
-      <div class="entry-post-nav group">
-          <?php $prev_post = get_previous_post(); ?>
+      <div id="nav-single">           
+        <div class="nav-previous"><?php previous_post_link('&#8249; %link', '%title', 'Next post in category', TRUE); ?> </div>
+        <div class="nav-next"><?php next_post_link('%link &#8250;', '%title', 'Next post in category', TRUE); ?> </div>         
+      </div><!-- end of #nav-single -->'
 
-          <?php if ( !empty( $prev_post ) ) : ?>  
-              <div class="post-prev">
-                  <a href="<?php echo get_permalink( $prev_post->ID ); ?>">? Prev Posts</a>
-                  <h4><a href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo $prev_post->post_title; ?></a></h4>
-              </div>
-          <?php endif; ?>
-
-          <?php $next_post = get_next_post(); ?>
-
-          <?php if ( !empty( $next_post ) ) : ?>  
-              <div class="post-next">
-                  <a href="<?php echo get_permalink( $next_post->ID ); ?>">Next Posts ?</a>
-                  <h4><a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo $next_post->post_title; ?></a></h4>
-              </div>
-          <?php endif; ?>
-      </div>
     </div>
     
   </div>
