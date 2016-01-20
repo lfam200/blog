9e8474697dfe562cf81b810879f801e9600ca113
Luis Abanto <lfam200@hotmail.com>
2016-01-19 19:54:58 -0500

    wsp12

--- themes/ravers/functions.php
+++ themes/ravers/functions.php
@@ -4,7 +4,7 @@ add_theme_support('post-thumbnails');
 
 add_image_size('my-size', 200, 180, true);
 add_image_size('profile', 144, 192, true);
-add_image_size('destacado_post', 720, 240, true);
+add_image_size('destacado_post', 1500, 850, true);
 
 function register_my_menus(){
 
--- themes/ravers/single.php
+++ themes/ravers/single.php
@@ -5,15 +5,18 @@
     <div class="row padding-top-lg padding-bottom">
       <div class="col-xs-12 rowsombra">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <div class="col-xs-10 col-xs-offset-1">
+          <div class="col-xs-10 col-xs-offset-1 margin-top">
             <h2 class="text-center"><?php the_title(); ?></h2>
             <p class="text-center">
               <small>
-                              Blog Home | <?php the_time('m.s.Y'); ?> By <?php the_author(', '); ?> | Updates
-                          </small>
+                <a href="<?php bloginfo('home'); ?>/blog">Blog Home</a> | <?php the_time('m.s.Y'); ?> By <?php the_author(', '); ?> | Updates
+               </small>
             </p>
-            <div class="col-xs-8 col-xs-offset-2">
-              <?php the_post_thumbnail(); ?>
+            <div class="col-xs-12">
+            <figure class="img-responsive">
+              <?php the_post_thumbnail('destacado_post', array('class' => 'img img-responsive')); ?>
+            </figure>
+              
             </div>
             <div class="col-xs-10 col-xs-offset-1 margin-top-lg margin-bottom-lg">
               <?php the_content(); ?>
@@ -29,7 +32,27 @@
                 <h2>pagina no encontrada</h2>
          
             <?php endif; ?>
+            
       </div> 
+      <div class="entry-post-nav group">
+          <?php $prev_post = get_previous_post(); ?>
+
+          <?php if ( !empty( $prev_post ) ) : ?>  
+              <div class="post-prev">
+                  <a href="<?php echo get_permalink( $prev_post->ID ); ?>">? Prev Posts</a>
+                  <h4><a href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo $prev_post->post_title; ?></a></h4>
+              </div>
+          <?php endif; ?>
+
+          <?php $next_post = get_next_post(); ?>
+
+          <?php if ( !empty( $next_post ) ) : ?>  
+              <div class="post-next">
+                  <a href="<?php echo get_permalink( $next_post->ID ); ?>">Next Posts ?</a>
+                  <h4><a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo $next_post->post_title; ?></a></h4>
+              </div>
+          <?php endif; ?>
+      </div>
     </div>
     
   </div>
