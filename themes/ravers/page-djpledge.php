99f72fb50ae59d9ab3b4c876fbd273581bfcd70c
Luis Abanto <lfam200@hotmail.com>
2016-01-29 18:53:37 -0500

    calendaar11111ui

---
+++ themes/ravers/page-djpledge.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Template Name: Página DJ Pledge
+*/
+?>
+<?php get_header(); ?>
+<div class="white">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xs-5 col-xs-offset-1 margin-top">
+        <p><h3 class="margin-top-lg">Wheter you just want to pledge your birthday to your fans or you will throw a private party, if you accept the challenge you can rise your birthday gifts to bring clean water in schools, orphanages an communities around the world. You just have to choose which tipe of project you'd like to support.</h3 class="margin-top-lg"></p>
+      </div>
+      <div class="col-xs-5 col-xs-offset-1">
+        <img src="<?php bloginfo('template_directory'); ?>/img/movil.png" alt="" class="img img-responsive">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="green">
+  <img src="<?php bloginfo('template_directory'); ?>/img/dj1.png" alt="" class="img img-responsive" width="100%">
+</div>
+<div class="green">
+  <img src="<?php bloginfo('template_directory'); ?>/img/dj2.png" alt="" class="img img-responsive" width="100%">
+</div>
+
+<div class="blue">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1 class="text-center texto-blanco margin-top-lg margin-bottom-lg">Choose you cause!</h1>
+      </div>
+      <div class="col-xs-12">
+        <div class="col-xs-5 col-xs-offset-1">
+          <div class="col-xs-12">
+            <h3 class="text-center texto-blanco">Build Wells + water purifiers</h3>
+          </div>
+          <div class="col-xs-12">
+            <img src="<?php bloginfo('template_directory'); ?>/img/wells.jpg" alt="" width="70%"class="img img-circle img-responsive center-block">
+          </div>
+          <div class="col-xs-8 col-xs-offset-2 margin-top">
+            <p class="text-justify texto-blanco">There is thousands of water well projects stalled because other organizations run out of funds. Let's help fulfill those water projects with your gifts. </p>
+          </div>
+        </div>
+        <div class="col-xs-5">
+          <div class="col-xs-12">
+            <h3 class="text-center texto-blanco">Free water refill stations</h3>
+          </div>
+          <div class="col-xs-12">
+            <img src="<?php bloginfo('template_directory'); ?>/img/elkay.jpg" alt="" width="70%"class="img img-circle img-responsive center-block">
+          </div>
+          <div class="col-xs-8 col-xs-offset-2 margin-top">
+            <p class="text-justify texto-blanco">With elkay water filters we can build free water refill stations in the poorest shools in developing countries.</p>
+          </div>
+        </div>
+      <div class="col-xs-12 margin-top  margin-bottom">
+        <div class="col-xs-5 col-xs-offset-1 margin-bottom-lg">
+          <button class="btn btn-default btn-lg center-block">Pledge my birthday</button>
+        </div>
+        <div class="col-xs-5 margin-bottom-lg">
+          <button class="btn btn-default btn-lg center-block">Pledge my birthday</button>
+        </div>
+        <div class="col-xs-5 col-xs-offset-1">
+          <button class="btn btn-default btn-lg center-block">Private Party</button>
+        </div>
+        <div class="col-xs-5">
+          <button class="btn btn-default btn-lg center-block">Private Party</button>
+        </div>
+      </div>  
+        
+      </div>
+    </div>
+  </div>
+</div>
+<div class="pinki">
+  <img src="<?php bloginfo('template_directory'); ?>/img/dj4.png" alt="" class="img img-responsive" width="100%">
+</div>
+<div class="white">
+  <img src="<?php bloginfo('template_directory'); ?>/img/dj5.png" alt="" class="img img-responsive margin-top-lg" width="100%">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 margin-top-lg margin-bottom-lg">
+        <button class="btn btn-primary text-center center-block btn-lg">READ THE BIRTHDAY'S FAQS</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
+</body>
+</html>
