a9d838fa182310c25a7d571a60b6dded0475aa9c
Luis Abanto <lfam200@hotmail.com>
2016-02-15 11:57:06 -0500

    responsive

--- themes/ravers/page-blog.php
+++ themes/ravers/page-blog.php
@@ -15,10 +15,10 @@ Template Name: Página Blog
   <div class="container">
     <div class="row rowsombra">
       <div class="col-xs-12">
-        <div class="cat col-xs-2 texto-blanco text-center">Categories</div>
+        <div class="cat col-xs-12 col-sm-12 col-md-6 col-lg-3 texto-blanco text-center">Categories</div>
       </div>
 
-      <div class="col-xs-8">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 
         <div class="title_border col-xs-12"></div>
         <div class="col-xs-12 categories_blog">
@@ -57,7 +57,7 @@ Template Name: Página Blog
         
       </div>
 
-      <div class="col-xs-4">
+      <div class="col-md-4 col-lg-4 hidden-xs hidden-sm ">
         <div class="col-xs-12">
           <div class="margin-top embed-responsive embed-responsive-16by9">
             <iframe class="center-block shadow embed-responsive-item" width="70%" src="https://www.youtube.com/embed/qOLZTZpjXQo" frameborder="0" allowfullscreen></iframe>
@@ -83,7 +83,7 @@ Template Name: Página Blog
 
 <script>
   $(document).ready(function(){
-    $('.cat-item').addClass("col-xs-4")
+    $('.cat-item').addClass("col-xs-12 col-sm-12 col-md-4 col-lg-4")
   });
 </script>
 </body>
